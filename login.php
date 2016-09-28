b2d523b08f8e635b5240602480233f0c9c2b84dc
ukupode <ukupode@greeny.cs.tlu.ee>
2016-09-28 15:57:49 +0300

    username and gender error

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
 	
 	
 	
-	
+
 	$signupEmailError = "";
 	$signupEmail = "";
 	$gender = "female";
@@ -32,7 +32,8 @@
 	
 	
 	
-	
+	$gender = "";
+	$genderError = "";
 	
 	$signupPasswordError = "";
 
@@ -47,28 +48,42 @@
 		{
 				$signupPasswordError="Parool peab olema vähemalt 8 tähemärki pikk.";
 		}
+		
+		if (isset($_POST["gender"]))
+		{
+			
+		}
+		else
+		{
+			
+			$genderError = "Vali üks kolmest";
+		}	
+	
 	}	
 	
 	
+	$signupUsernameError = "";
 	
+	if (isset($_POST["signupUsername"]))
+	{
+			if (empty($_POST["signupUsername"]))
+			{
+				$signupUsernameError= "Väli on kohustuslik";	
+			}	
+			else
+			{
+				$signupUsername = $_POST["signupUsername"];
+			
+			}	
+	}	
 	
-	
-	
-	$gender = "";
-	
-	if (isset ($_POST["gender"])) {
-		if (empty ($_POST["gender"])) {
-			$genderError = "* Väli on kohustuslik!";
-		} else {
-			$gender = $_POST["gender"];
-		}
-		
-	} 
-	
+
 	if ($signupEmailError == "" &&
 		$signupPasswordError == "" &&
+		$signupUsernameError == "" &&
 		isset ($_POST["signupEmail"]) &&
-		isset ($_POST["signupPassword"])
+		isset ($_POST["signupPassword"]) &&
+		isset ($_POST["signupUsername"])
 			
 			
 		)
@@ -81,7 +96,7 @@
 	}
 	
 	if(isset($_POST["loginEmail"]) && 
-	isset($_POST["loginPassword"]) && 
+	isset($_POST["loginPassword"]) &&
 	!empty($_POST["loginEmail"]) && 
 	!empty($_POST["loginPassword"]))
 	{
@@ -117,31 +132,31 @@
 		
 	<h1> sign up </h1>
 	<form method="POST">
-		<input name="signupUsername" placeholder="Username" type="Username">
+		<input name="signupUsername" placeholder="Username" type="Username"><?php echo $signupUsernameError; ?>
 		<br>
 		<input name="signupEmail" placeholder="näide@näide.ee" type="email" value="<?=$signupEmail;?>"><?php echo $signupEmailError; ?>
 		<br>
-		<input name="signupPassword" placeholder="parool" type="password"><?php echo $signupPasswordError; ?>
+		<input name="signupPassword" placeholder="parool(vähemalt 8 pikk)" type="password"><?php echo $signupPasswordError; ?>
 		<br>
 		
 		
 		<?php if ($gender == "female") { ?>
-			<input type="radio" name="gender" value="female"> female<br>
+			<input type="radio" name="gender" value="female"> female <?=$genderError;?><br>
 		<?php } else { ?>
-			<input type="radio" name="gender" value="female" > female<br>
+			<input type="radio" name="gender" value="female" > female <?=$genderError;?><br>
 		<?php } ?>
 		
 		<?php if ($gender == "male") { ?>
-			<input type="radio" name="gender" value="male"> male<br>
+			<input type="radio" name="gender" value="male"> male <?=$genderError;?><br>
 		<?php } else { ?>
-			<input type="radio" name="gender" value="male" > male<br>
+			<input type="radio" name="gender" value="male" > male <?=$genderError;?><br>
 		<?php } ?>
 		
 		
 		<?php if ($gender == "other") { ?>
-			<input type="radio" name="gender" value="other" checked> other<br>
+			<input type="radio" name="gender" value="other"> other <?=$genderError;?><br>
 		<?php } else { ?>
-			<input type="radio" name="gender" value="other" checked> other<br>
+			<input type="radio" name="gender" value="other"> other <?=$genderError;?><br>
 		<?php } ?>
 		<br>
 	
